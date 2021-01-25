d0e621baa4adb9b325258b64e20a6e40ff94640d
Alex Huijsman <alexkorea@gmail.com>
2021-01-24 21:28:17 -0800

    improve release script and tasks

--- sh/tag-and-zip-for-release.sh
+++ sh/tag-and-zip-for-release.sh
@@ -1,7 +1,7 @@
 zipFile="$2/TournamentsEnhanced$1.zip"
 folderToZip="$2/TournamentsEnhanced"
-git tag $1
+git tag $1 --force
 git push
-git push origin $1
+git push origin $1 --force
 rm "$zipFile"
 7z a "$zipFile" "$folderToZip"
