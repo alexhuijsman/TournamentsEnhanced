fcff41294db4275a5991071c8da340deae19cc5f
Alex Huijsman <alexkorea@gmail.com>
2021-01-26 14:34:58 -0800

    fix some unit tests

--- src/Extensions/ListExtensions.cs
+++ src/Extensions/ListExtensions.cs
@@ -15,7 +15,7 @@ namespace TournamentsEnhanced
       while (n > 1)
       {
         n--;
-        k = MBMBRandom.DeterministicRandom.Next(n + 1);
+        k = MBMBRandom.DeterministicRandomInt(n + 1);
         T value = list[k];
         list[k] = list[n];
         list[n] = value;
