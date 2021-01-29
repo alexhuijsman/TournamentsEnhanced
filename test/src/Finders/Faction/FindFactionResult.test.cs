7f45ca519ed92a6c5ccb1e6fc1f7cee5f026c7cb
Alex Huijsman <alexkorea@gmail.com>
2021-01-29 12:05:08 -0800

    add unit tests

---
+++ test/src/Finders/Faction/FindFactionResult.test.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using TournamentsEnhanced;
+using TournamentsEnhanced.Finder;
+using TournamentsEnhanced.Wrappers.CampaignSystem;
+
+
+namespace Test
+{
+  public class FindFactionResultTest
+  {
+    [Test]
+    public void Success_ShouldReturnSuccess()
+    {
+      var expectedNominees = new List<MBFaction>();
+      var result = FindFactionResult.Success(expectedNominees);
+
+      result.Status.ShouldBe(ResultStatus.Success);
+    }
+
+    [Test]
+    public void Success_ShouldReturnExpectedCandidates()
+    {
+      var expectedNominees = new List<MBFaction>();
+      var result = FindFactionResult.Success(expectedNominees);
+
+      result.AllQualifiedCandidates.ShouldBe(expectedNominees);
+    }
+
+  }
+}
