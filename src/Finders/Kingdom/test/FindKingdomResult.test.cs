e7b9017f0fa258d5344dbdb8f881b1fe707f67e6
Alex Huijsman <alexkorea@gmail.com>
2021-02-06 09:35:11 -0800

    add unit tests

---
+++ src/Finders/Kingdom/test/FindKingdomResult.test.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using TournamentsEnhanced;
+using TournamentsEnhanced.Finder;
+using TournamentsEnhanced.Wrappers.CampaignSystem;
+
+
+namespace Test
+{
+  public class FindKingdomResultTest : TestBase
+  {
+    [Test]
+    public void Success_ShouldReturnSuccess()
+    {
+      var expectedNominees = new List<MBKingdom>();
+      var result = FindKingdomResult.Success(expectedNominees);
+
+      result.Status.ShouldBe(ResultStatus.Success);
+    }
+
+    [Test]
+    public void Success_ShouldReturnExpectedCandidates()
+    {
+      var expectedNominees = new List<MBKingdom>();
+      var result = FindKingdomResult.Success(expectedNominees);
+
+      result.AllQualifiedCandidates.ShouldBe(expectedNominees);
+    }
+
+  }
+}
