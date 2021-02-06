40c4c02181d7f262b28a6f175b35073b16ed2642
Alex Huijsman <alexkorea@gmail.com>
2021-02-06 11:26:56 -0800

    exclude cached non-target bannerlord dlls

--- sh/switch-target-bannerlord-lib.sh
+++ sh/switch-target-bannerlord-lib.sh
@@ -2,5 +2,6 @@ gameVersion=$1
 versionedLibFolder=lib/Bannerlord/$gameVersion
 targetLibFolder=lib/Bannerlord/target
 rm -rf $targetLibFolder
-mkdir $targetLibFolder
+mkdir -p $targetLibFolder
 cp $versionedLibFolder/*.dll $targetLibFolder
+echo Switched target to $gameVersion.
