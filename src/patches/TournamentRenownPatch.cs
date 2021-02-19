473c985b1e8eb02f6354ea806d32c1726624f09f
3ger <41837307+3ger@users.noreply.github.com>
2021-02-19 05:22:34 +0100

    Fixed perk Duelist -> giving double renown for win

--- src/patches/TournamentRenownPatch.cs
+++ src/patches/TournamentRenownPatch.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 
 namespace TournamentsEnhanced
@@ -9,6 +11,11 @@ namespace TournamentsEnhanced
     static void Postfix(ref int __result)
     {
       __result = TournamentsEnhancedSettings.Instance.RenownReward;
+      if (winner.GetPerkValue(DefaultPerks.OneHanded.Duelist))
+      {
+        // needs to add it, since SecondaryBonus = 1 (for now at least)
+        __result += (int)Math.Round(__result * DefaultPerks.OneHanded.Duelist.SecondaryBonus);
+      }
     }
   }
 }
