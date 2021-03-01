44e3f2e92af6368a3da9ae1634cd6a4d0c7faec4
Alex Huijsman <alexkorea@gmail.com>
2021-03-01 09:35:53 -0800

    bump version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cda89b54-0795-4e72-9bbe-49220be96d17")]
-[assembly: AssemblyFileVersion("1.5.7.16")]
-[assembly: AssemblyVersion("1.5.7.16")]
+[assembly: AssemblyFileVersion("1.5.7.17")]
+[assembly: AssemblyVersion("1.5.7.17")]
