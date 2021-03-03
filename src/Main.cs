34ca69f587b4a738e9b1a8373a01750e57b93dd2
Alex Huijsman <alexkorea@gmail.com>
2021-03-03 12:50:14 -0800

    add TournamentFairArmour compatibility fix

--- src/Main.cs
+++ src/Main.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using HarmonyLib;
+using SandBox;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TournamentsEnhanced.TeamTournament;
 
 namespace TournamentsEnhanced
 {
@@ -14,12 +18,14 @@ namespace TournamentsEnhanced
     public static string ModuleName = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
     private static string ModuleVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
+    protected object _tournamentFairArmorSettingsCampaignBehavior;
+
     protected override void OnBeforeInitialModuleScreenSetAsRoot()
     {
       InformationManager.DisplayMessage(new InformationMessage($"Loaded {ModuleName} v{ModuleVersion}", Color.FromUint(4282569842U)));
 
-        var harmony = new Harmony(ModuleName);
-        harmony.PatchAll();
+      var harmony = new Harmony(ModuleName);
+      harmony.PatchAll();
     }
 
     protected override void OnGameStart(Game game, IGameStarter gameStarter)
@@ -27,8 +33,20 @@ namespace TournamentsEnhanced
       if (game.GameType is Campaign)
       {
         CampaignGameStarter campaignStarter = (CampaignGameStarter)gameStarter;
-
         campaignStarter.AddBehavior(new BehaviorBase());
+
+        if (Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+            .FirstOrDefault(c => c.FullName == "TournamentFairArmour") == null)
+        {
+          Type campaignStarterType = campaignStarter.GetType();
+          BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+          FieldInfo field = campaignStarterType.GetField("_campaignBehaviors", bindingFlags);
+          var campaignBehaviors = (List<CampaignBehaviorBase>)field.GetValue(gameStarter);
+          var overrideSpawnArmourMissionListenerType
+            = Type.GetType("TournamentFairArmour.OverrideSpawnArmourMissionListener");
+          _tournamentFairArmorSettingsCampaignBehavior
+            = campaignBehaviors.FirstOrDefault(b => b.GetType().Equals(campaignStarterType));
+        }
       }
     }
 
@@ -39,6 +57,37 @@ namespace TournamentsEnhanced
         BehaviorBase.weeksSinceHost = 1;
         Utilities.CreateInitialTournaments();
       }
+    }
+
+    public override void OnMissionBehaviourInitialize(Mission mission)
+    {
+      if (_tournamentFairArmorSettingsCampaignBehavior != null && mission.HasMissionBehaviour<TeamTournamentMissionController>())
+      {
+        AddOverrideSpawnArmourMissionListener(mission);
+      }
+    }
+
+    private void AddOverrideSpawnArmourMissionListener(Mission mission)
+    {
+      var equipment = GetEquipmentByCultureOfCurrentSettlement();
+      if (equipment != null)
+      {
+        IMissionListener overrideSpawnArmourMissionListener =
+          (IMissionListener)Activator.CreateInstance(
+            "TournamentFairArmour",
+            "TournamentFairArmour.OverrideSpawnArmourMissionListener",
+            new object[] { mission });
+
+        mission.AddListener(overrideSpawnArmourMissionListener);
+      }
+    }
+
+    private Equipment GetEquipmentByCultureOfCurrentSettlement()
+    {
+      var cultureStringId = Settlement.CurrentSettlement?.Culture?.StringId;
+      var type = Type.GetType("TournamentFairArmour.OverrideSpawnArmourMissionListener");
+      MethodInfo methodInfo = type.GetMethod("GetEquipmentOrDefaultIfDisabled");
+      return (Equipment)methodInfo.Invoke(_tournamentFairArmorSettingsCampaignBehavior, new object[] { cultureStringId });
     }
   }
 }
